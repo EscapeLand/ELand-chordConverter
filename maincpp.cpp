d54d49956f2c938766168ad3ab93458319209584
JamzumSum <zzzzss990315@gmail.com>
2018-03-06 22:46:53 +0800

    Update maincpp.cpp

--- maincpp.cpp
+++ maincpp.cpp
@@ -1,78 +1,9 @@
-#include "cv.h"
-#include "highgui.h"
-#include "opencv2/opencv.hpp"
-#include "opencv2/imgproc.hpp"
-#include <iostream>
-extern cv::Mat threshold(std::string);
-extern bool isEmptyLine(cv::Mat img, int y, double rate);
-extern cv::Mat trim(cv::Mat img, double threshold = 0.01) ;
-
-typedef struct _space{
-	int start;
-	int length;
-}space;
-
-extern bool* KClassify(std::vector<space> collection);
-extern void interCheck(std::vector<space> *collection);
-
-int main() {
-	std::string f;
-	std::cin >> f;
-	
-	cv::Mat img = threshold(f);
-	std::vector<space> coll;
-	cv::Mat trimmed = trim(img);
-	
-	bool flag = false;
-	for (int st = trimmed.rows,i = 0; i < trimmed.rows; i++) {
-		//初步设为0.04 以后再说
-		if (isEmptyLine(trimmed, i, 0)) {
-			if (!flag) {
-				st = i;
-				flag = true;
-			}
-		}
-		else {
-			if (flag) {
-				coll.push_back({ st,i - 1 - st });
-				flag = false;
-			}
-		}
-	}
-	size_t n;
-	do {
-		n = coll.size();
-		interCheck(&coll);
-	} while (n > coll.size());
-	bool* r = KClassify(coll);
-	std::vector<space> toCut;
-	for (int i = 0; i < n; i++) {
-		if (r[i]) {#include "cv.h"
-#include "highgui.h"
-#include "opencv2/opencv.hpp"
-#include "opencv2/imgproc.hpp"
-#include <iostream>
-
-#define SUCCEED(x) (x>=0)
-
-extern cv::Mat threshold(std::string);
-extern bool isEmptyLine(cv::Mat img, int y, double rate);
-extern cv::Mat trim(cv::Mat img, double threshold = 0.01) ;
-
-typedef struct _space{
-	int start;
-	int length;
-}space;
-
-extern bool* KClassify(std::vector<space> collection);
-extern void interCheck(std::vector<space> *collection);
-extern int interCheck(std::vector<space> *collection, bool reserved);
+#include"myheader.h"
 
 int main() {
 	std::cout << "Drag image here to start: "<< std::endl ;
 	std::string f;
 	std::cin >> f;
-	
 	cv::Mat img = threshold(f);
 	if (img.empty()) {
 		std::cout << "Wrong format." << std::endl;
@@ -97,6 +28,40 @@ int main() {
 			}
 		}
 	}
+	bool cat = true;
+	if (!coll.size()) {
+		cat = false;
+		std::cout << "裁剪失败，等待二次裁剪" << std::endl;
+		//TODO
+		std::vector<cv::Vec4i> rows;
+		findRow(trimmed,CV_PI/18,rows);
+		if (rows.size() > 5) {
+			std::cout << "二次裁剪开始." << std::endl;
+			flag = false;
+			for (int st = trimmed.rows, i = 0; i < trimmed.rows; i++) {
+				if (isEmptyLine(trimmed, i,std::max(std::min(rows[0][0],rows[0][2]),std::min(rows[5][0],rows[5][2])),
+					std::min(std::max(rows[0][0], rows[0][2]), std::max(rows[5][0], rows[5][2])), 0)) {
+					if (!flag) {
+						st = i;
+						flag = true;
+					}
+				}
+				else {
+					if (flag) {
+						coll.push_back({ st,i - 1 - st });
+						flag = false;
+					}
+				}
+			}
+			
+		}
+		if (!coll.size()) {
+			std::cout << "二次裁剪失败，请手动处理" << std::endl;
+			imshow("WTF", trimmed);
+			cvWaitKey();
+			return 1;
+		}
+	}
 	size_t n;
 	do {
 		n = coll.size();
@@ -106,8 +71,12 @@ int main() {
 	std::vector<space> toCut;
 	for (int i = 0; i < n; i++) {
 		if (r[i]) {
-			//line(trimmed, CvPoint(0, coll[i].start), CvPoint(trimmed.cols, coll[i].start), CvScalar(0, 0, 255));
-			//line(trimmed, CvPoint(0, coll[i].start+ coll[i].length), CvPoint(trimmed.cols, coll[i].start + coll[i].length), CvScalar(0, 0, 255));
+			cv::Mat ccolor;
+			cvtColor(img, ccolor, CV_GRAY2BGR);
+			line(ccolor, CvPoint(0, coll[i].start), CvPoint(trimmed.cols, coll[i].start), CvScalar(0, 0, 255));
+			line(ccolor, CvPoint(0, coll[i].start + coll[i].length), CvPoint(trimmed.cols, coll[i].start + coll[i].length), CvScalar(0, 0, 255));
+			imshow("2", ccolor); cvWaitKey(); 
+			
 			toCut.push_back(coll[i]);
 		}
 	}
@@ -126,7 +95,7 @@ int main() {
 	for (int i = 1; i < toCut.size(); i++) piece[i] = trimmed(cv::Range(toCut[i - 1].start + toCut[i - 1].length + 1, toCut[i].start), cv::Range(0, trimmed.cols));
 	piece[toCut.size()] = trimmed(cv::Range(toCut[toCut.size() - 1].start + toCut[toCut.size() - 1].length + 1, trimmed.rows), cv::Range(0, trimmed.cols));
 	
-	if (dog) {
+	if (dog && cat) {
 		std::cout << "运行修补算法" << std::endl;
 		std::vector<space> toJoin;
 		for (int i = 1; i <= toCut.size(); i++) toJoin.push_back({0,piece[i].rows});
@@ -142,7 +111,7 @@ int main() {
 			else b[i] = r[k++];
 		}
 		delete[] r;
-		for (int i = toCut.size(); i > 0; i--) {
+		for (size_t i = toCut.size(); i > 0; i--) {
 			if (!b[i]) {
 				//将piece[i]与piece[i-1]拼接在一起
 				vconcat(piece[i-1],piece[i], piece[i - 1]);
@@ -151,35 +120,46 @@ int main() {
 		}
 		delete[] b;
 	}
-	cvNamedWindow("2",1);
+	std::vector<cv::Mat> chords;
+	std::vector<cv::Mat> section;
+	std::vector<cv::Mat> info;
+	std::vector<cv::Mat> notes;
+	flag = false;
 	for (int i = 0; i <= toCut.size(); i++) {
 		if (piece[i].empty()) continue;
-		imshow("2", piece[i]);
-		cvWaitKey(1000);
-	}
-	
-	delete[] piece;
-	
-	cvWaitKey();
-}
-			//line(trimmed, CvPoint(0, coll[i].start), CvPoint(trimmed.cols, coll[i].start), CvScalar(0, 0, 255));
-			//line(trimmed, CvPoint(0, coll[i].start+ coll[i].length), CvPoint(trimmed.cols, coll[i].start + coll[i].length), CvScalar(0, 0, 255));
-			toCut.push_back(coll[i]);
+		std::vector<cv::Vec4i> rows;
+		findRow(piece[i], CV_PI / 18, rows);
+		
+		if (rows.size() > 5) {
+			flag = true;
+			chords.push_back(piece[i]);
+
+			std::vector<cv::Vec4i> lines;
+			int max = std::min(rows[5][1],rows[5][3]);
+			int min = std::max(rows[0][1],rows[0][3]);
+			findCol(piece[i], CV_PI / 18 * 8, max , min , lines);
+			for (int j = 0; j < lines.size() - 1; j++) {
+				section.push_back(piece[i](cv::Range(0, piece[i].rows), cv::Range(lines[j][0]+1, lines[j + 1][0])).clone());
+			}
+			
+		}
+		else {
+			if(flag) notes.push_back(piece[i]);
+			else info.push_back(piece[i]);
 		}
 	}
-	coll.clear();
-	delete[] r;
-	cv::Mat *piece = new cv::Mat[toCut.size()+1];
-	piece[0] = trimmed(cv::Range(0, toCut[0].start), cv::Range(0, trimmed.cols));
-	for (int i = 1; i < toCut.size(); i++) piece[i] = trimmed(cv::Range(toCut[i - 1].start + toCut[i - 1].length + 1, toCut[i].start), cv::Range(0, trimmed.cols));
-	piece[toCut.size()] = trimmed(cv::Range(toCut[toCut.size() - 1].start + toCut[toCut.size() - 1].length + 1, trimmed.rows), cv::Range(0, trimmed.cols));
-	
-	cvNamedWindow("2",1);
-	for (int i = 0; i <= toCut.size(); i++) {
-		imshow("2", piece[i]);
+	delete[] piece;
+	for (int i = 0; i < section.size();i++) {
+		imshow("section",section[i]);
 		cvWaitKey(1000);
 	}
-	delete[] piece;
-	system("pause");
+	cvDestroyWindow("section");
+	for (cv::Mat& i : notes) {
+		imshow("notes",i);
+		cvWaitKey(1000);
+	}
+	cvWaitKey();
+	cvDestroyAllWindows();
 	
 }
+
